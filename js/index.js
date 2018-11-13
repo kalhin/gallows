d6d3761816753a860f2c9751f677d3eecd9111f7
Roman Kalgin <kalginroman@gmail.com>
2018-11-13 23:15:28 +0000

    did main functional

--- js/index.js
+++ js/index.js
@@ -1,35 +1,56 @@
+//приховання кнопки "ПОЧАТОК ГРИ" та відображення контейнера при натисненні кнопки 
+document.getElementById("btn-start").addEventListener("click", toShowContainer);
+function toShowContainer(){
+    document.getElementById('container').style.display = "block";
+    document.getElementById('btn-start').style.display = "none";
+}
+
 //набір довільних слів (змінити на базу)
-const words = ["курча", "кат", "собака"];
+const words = ["сад", "кат", "ара"];
 
 //рандомний вибір слова із масиву (бази)
 let randomNumber = Math.floor(Math.random() * words.length);
 let randomWord = words[randomNumber];
 
-//виведення вибраного слова 
-//document.write(randomWord);
+//початок гри
+document.getElementById("btn-start").addEventListener("click", startGame);
+function startGame(){
 
-//заміна літер у слові на нижнє підкреслення та виведення на сторінку
-let coverWord = [];
-for (let i = 0; i < randomWord.length; i++) {
-    coverWord += "_ ";
-}
-document.write(coverWord);
-
-//порівняння літери користувача із літерами рандомного слова
-document.getElementById("btn").addEventListener("click", compareValueBtnWithLetterInRandomWord);
-
-function compareValueBtnWithLetterInRandomWord(){
-    let coverWordChange = [];
+    //заміна літер у слові на нижнє підкреслення та виведення на сторінку
+    let coverWord = "";
     for (let i = 0; i < randomWord.length; i++) {
-        if (randomWord[i] === "a") { //замість "а" потрібно вибрати value кнопки btn
-            coverWordChange += "a "; //замість "а" потрібно вибрати value кнопки btn
-        } else {
-            coverWordChange += "_ ";
-        }
+        coverWord += "_";
     }
-    document.write(coverWordChange);
+    let exWord = document.querySelector('p');
+    exWord.textContent = coverWord;
+    
+    //порівняння літер користувача із літерами рандомного слова
+    document.getElementById("btn").addEventListener("click", compareValueBtnWithLetterInRandomWord);
+    
+    function compareValueBtnWithLetterInRandomWord(){
+        const btnValue = document.activeElement.textContent;
+        let coverWordNew = "";
+        for (let i = 0; i < randomWord.length; i++) {
+            if (btnValue === randomWord[i]) {
+                coverWordNew += btnValue;
+            } else if(document.querySelector("p").innerText[i] === "_"){
+                 coverWordNew += "_";
+            } else {
+               coverWordNew += randomWord[i];
+            }
+        }
+        exWord.textContent = coverWordNew;
+        //дезактивує натиснуту кнопку:
+        document.activeElement.disabled = true;
+    } 
+
+    ////завершення гри
+    //if (document.querySelector("p").innerText === randomWord) {
+    //    alert("вітаю з перемогою")
+    //    //виклик вікна "вітаю з перемогою" та пропозиція зіграти ще
+    //} else {
+    //    alert("ви програли")
+    //    //виклик вікна "ви програли" та пропозиція зіграти ще
+    //}
 }
-
-
-
 
