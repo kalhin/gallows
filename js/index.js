58f08bf26d8fd2c3c191c06d24a67f0dec759579
Roman Kalgin <kalginroman@gmail.com>
2018-11-14 17:20:17 +0000

    add counter

--- js/index.js
+++ js/index.js
@@ -6,51 +6,54 @@ function toShowContainer(){
 }
 
 //набір довільних слів (змінити на базу)
-const words = ["сад", "кат", "ара"];
+const words = ["кіт", "пес"];
 
 //рандомний вибір слова із масиву (бази)
 let randomNumber = Math.floor(Math.random() * words.length);
 let randomWord = words[randomNumber];
 
-//початок гри
-document.getElementById("btn-start").addEventListener("click", startGame);
-function startGame(){
-
-    //заміна літер у слові на нижнє підкреслення та виведення на сторінку
-    let coverWord = "";
-    for (let i = 0; i < randomWord.length; i++) {
-        coverWord += "_";
-    }
-    let exWord = document.querySelector('p');
-    exWord.textContent = coverWord;
-    
-    //порівняння літер користувача із літерами рандомного слова
-    document.getElementById("btn").addEventListener("click", compareValueBtnWithLetterInRandomWord);
-    
-    function compareValueBtnWithLetterInRandomWord(){
-        const btnValue = document.activeElement.textContent;
-        let coverWordNew = "";
-        for (let i = 0; i < randomWord.length; i++) {
-            if (btnValue === randomWord[i]) {
-                coverWordNew += btnValue;
-            } else if(document.querySelector("p").innerText[i] === "_"){
-                 coverWordNew += "_";
-            } else {
-               coverWordNew += randomWord[i];
-            }
-        }
-        exWord.textContent = coverWordNew;
-        //дезактивує натиснуту кнопку:
-        document.activeElement.disabled = true;
-    } 
-
-    ////завершення гри
-    //if (document.querySelector("p").innerText === randomWord) {
-    //    alert("вітаю з перемогою")
-    //    //виклик вікна "вітаю з перемогою" та пропозиція зіграти ще
-    //} else {
-    //    alert("ви програли")
-    //    //виклик вікна "ви програли" та пропозиція зіграти ще
-    //}
+//заміна літер у слові на нижнє підкреслення та виведення на сторінку
+let coverWord = "";
+for (let i = 0; i < randomWord.length; i++) {
+    coverWord += "_";
 }
+let exWord = document.querySelector('p');
+exWord.textContent = coverWord;
 
+//порівняння літер користувача із літерами рандомного слова
+document.getElementById("btn").addEventListener("click", compareValueBtnWithLetterInRandomWord);
+
+function compareValueBtnWithLetterInRandomWord(){
+    const btnValue = document.activeElement.textContent;
+    let coverWordNew = "";
+    for (let i = 0; i < randomWord.length; i++) {
+        if (btnValue === randomWord[i]) {
+            coverWordNew += btnValue;
+        } else if (document.querySelector("p").innerText[i] === "_"){
+             coverWordNew += "_";
+        } else {
+           coverWordNew += randomWord[i];
+        }
+    }
+    exWord.textContent = coverWordNew;
+    //дезактивує натиснуту кнопку:
+    document.activeElement.disabled = true;
+} 
+//підрахунок кількості спроб (5) та завершення гри
+document.getElementById("btn").addEventListener("click", counter);
+   
+let count = 5;
+function counter(){
+    if (count >= 0) {
+        document.getElementById("counter").textContent = count;
+    }
+        count--;
+    if (document.getElementById("counter").innerText === "0"){
+        $("#exampleModalCenter").modal("show");
+        document.getElementById("modalText").textContent = "Грайте далі і все вийде";
+    }
+    if (document.querySelector("p").innerText === randomWord){
+        $("#exampleModalCenter").modal("show");
+        document.getElementById("modalText").textContent = "Вітання! Ви справжній ерудит";
+    }
+}
